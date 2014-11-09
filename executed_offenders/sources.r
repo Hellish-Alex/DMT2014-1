deba2ca2130f6a7f7c75b5049e759a46661168c2
m10gda <gritsayda@gmail.com>
2014-11-09 04:35:17 +0300

    solution

---
+++ executed_offenders/sources.r
@@ -0,0 +1,25 @@
+### Including of packages
+
+library(XML)
+library(tm)
+library(SnowballC) 
+library(wordcloud)
+library(data.table)
+
+### Sources
+#load("executed.RData")
+source("read_data.R")
+source("vocabulary.R")
+source("repentance.R")
+source("visualisation.R")
+
+#save.image("executed.RData")
+
+
+
+
+
+
+
+
+
